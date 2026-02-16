5f18e7d34da19319549e6b6e9845abdf4882dc06
Muhammad Danial Khilji <muhammaddanial.khilji@choreograph.com>
2026-02-16 13:44:14 +0000

    Added intro page with profile picture

---
+++ src/data/intro.ts
@@ -0,0 +1,38 @@
+export interface SocialLink {
+  label: string;
+  url: string;
+  icon: string;
+}
+
+export const intro = {
+  name: "Muhammad Danial Khilji",
+  title: "Data Scientist | Machine Learning Engineer",
+};
+
+export const socialLinks: SocialLink[] = [
+  {
+    label: "LinkedIn",
+    url: "https://www.linkedin.com/in/danialkhilji/",
+    icon: "bi:linkedin",
+  },
+  {
+    label: "GitHub",
+    url: "https://github.com/danialkhilji",
+    icon: "bi:github",
+  },
+  {
+    label: "Medium",
+    url: "https://medium.com/@khiljidanial",
+    icon: "bi:medium",
+  },
+  {
+    label: "ResearchGate",
+    url: "https://www.researchgate.net/profile/Danial-Khilji",
+    icon: "bi:journal-text",
+  },
+  {
+    label: "Email",
+    url: "mailto:danialkhilji@yahoo.com",
+    icon: "bi:envelope-fill",
+  },
+];
